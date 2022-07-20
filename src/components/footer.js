0b8116582d57dbf35929443d55c0288b73369cc4
BilliesHQ <cohen.raquel07@gmail.com>
2022-07-20 16:01:32 +0000

    update

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/footer.css';
+
+function Footer() {
+  return(
+    <div className='footer'>
+      <h1 className='header'>Information</h1>
+    </div>
+  );
+}
+
+export default Footer;
